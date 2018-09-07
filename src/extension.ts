24e1be89024a8aaaf312515b0cd95f35e4e608a8
Sathya Balaji <sathya.balaji@ip-10-2-41-2.ap-southeast-1.compute.internal>
2018-09-07 10:06:23 +0800

    feat(src/) execute formatter on save after enabling

--- src/extension.ts
+++ src/extension.ts
@@ -4,9 +4,11 @@ import {HclClient} from './hclClient';
 
 export function activate(context: vscode.ExtensionContext) {
 
+    let config = vscode.workspace.getConfiguration('hclfmt');
+    let client = new HclClient(config);
+
     let disposable = vscode.commands.registerCommand('extension.hclfmt', () => {
-        let config = vscode.workspace.getConfiguration('hclfmt');
-        let client = new HclClient(config);
+        
         // get the current active document and 
         // and apply the formater now.
         let editor = vscode.window.activeTextEditor;
@@ -15,12 +17,25 @@ export function activate(context: vscode.ExtensionContext) {
             client.formatFile(doc.fileName).then((success) => {
                 vscode.window.showInformationMessage("Format success!!");
             }).catch((err) => {
-                vscode.window.showInformationMessage("Format Failed!");
+                vscode.window.showInformationMessage("Format Failed!!");
             })
         }
     });
 
+    // apply the format operation on save event.
+    let saveDisposer = vscode.commands.registerCommand('extension.executeOnSave', () => {
+        const onSave = vscode.workspace.onDidSaveTextDocument((doc: vscode.TextDocument) => {
+            let fileName = doc.fileName;
+            client.formatFile(fileName).then((success) => {
+                vscode.window.showInformationMessage("Format Success!!");
+            }).then((err)=> {
+                vscode.window.showInformationMessage("Format Failed!!");
+            });
+        });
+    });
+
     context.subscriptions.push(disposable);
+    context.subscriptions.push(saveDisposer);
 }
 
 export function deactivate() {
