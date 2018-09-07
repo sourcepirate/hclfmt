6b4284a2534f1396061c82855cdd4afa2f70cd70
Sathya Balaji <sathya.balaji@ip-10-2-41-2.ap-southeast-1.compute.internal>
2018-09-07 09:49:04 +0800

    chore(src/) removed unwanted commentes

--- src/extension.ts
+++ src/extension.ts
@@ -1,20 +1,9 @@
 'use strict';
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import {HclClient} from './hclClient';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "hclfmt" is now active!');
-
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
     let disposable = vscode.commands.registerCommand('extension.hclfmt', () => {
         let config = vscode.workspace.getConfiguration('hclfmt');
         let client = new HclClient(config);
@@ -23,12 +12,9 @@ export function activate(context: vscode.ExtensionContext) {
         let editor = vscode.window.activeTextEditor;
         if(editor){
             let doc : vscode.TextDocument = editor.document;
-            console.log(doc.fileName);
             client.formatFile(doc.fileName).then((success) => {
-                // exitcode ==  0
                 vscode.window.showInformationMessage("Format success!!");
             }).catch((err) => {
-                // exitcode != 1
                 vscode.window.showInformationMessage("Format Failed!");
             })
         }
@@ -37,6 +23,5 @@ export function activate(context: vscode.ExtensionContext) {
     context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {
 }
