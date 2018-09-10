5b29cc128020f6ea244a2bfff7e4ea2281677d50
Sathya Balaji <sathya.balaji@ip-10-2-41-2.ap-southeast-1.compute.internal>
2018-09-10 10:35:51 +0800

    feat(src/) added autoformat hcl

--- src/extension.ts
+++ src/extension.ts
@@ -1,19 +1,28 @@
 'use strict';
 import * as vscode from 'vscode';
-import {HclClient} from './hclClient';
+import { HclClient } from './hclClient';
 
 export function activate(context: vscode.ExtensionContext) {
 
     let config = vscode.workspace.getConfiguration('hclfmt');
     let client = new HclClient(config);
 
+    vscode.workspace.onDidSaveTextDocument((doc: vscode.TextDocument) => {
+        let fileName = doc.fileName;
+        client.formatFile(fileName).then((success) => {
+            vscode.window.showInformationMessage("Format Success!!");
+        }).catch((err) => {
+            vscode.window.showInformationMessage("Format Failed!!");
+        });
+    });
+
     let disposable = vscode.commands.registerCommand('extension.hclfmt', () => {
-        
+
         // get the current active document and 
         // and apply the formater now.
         let editor = vscode.window.activeTextEditor;
-        if(editor){
-            let doc : vscode.TextDocument = editor.document;
+        if (editor) {
+            let doc: vscode.TextDocument = editor.document;
             client.formatFile(doc.fileName).then((success) => {
                 vscode.window.showInformationMessage("Format success!!");
             }).catch((err) => {
@@ -23,19 +32,8 @@ export function activate(context: vscode.ExtensionContext) {
     });
 
     // apply the format operation on save event.
-    let saveDisposer = vscode.commands.registerCommand('extension.executeOnSave', () => {
-        vscode.workspace.onDidSaveTextDocument((doc: vscode.TextDocument) => {
-            let fileName = doc.fileName;
-            client.formatFile(fileName).then((success) => {
-                vscode.window.showInformationMessage("Format Success!!");
-            }).then((err)=> {
-                vscode.window.showInformationMessage("Format Failed!!");
-            });
-        });
-    });
 
     context.subscriptions.push(disposable);
-    context.subscriptions.push(saveDisposer);
 }
 
 export function deactivate() {
